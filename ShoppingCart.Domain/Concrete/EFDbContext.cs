4d8c4dfea03777a90ee73c6c0400f90b7bce98bc
cicimen <egemengulkilik@gmail.com>
2013-11-06 00:27:55 +0200

    commit

---
+++ ShoppingCart.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,10 @@
+﻿using ShoppingCart.Domain.Entities;
+using System.Data.Entity;
+
+namespace ShoppingCart.Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
